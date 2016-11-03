73c4f3503d93fe240b7482443f4a45fa97718af0
EnesIdrisoglu <idrisoglu_enes@hotmail.com>
2016-11-03 19:30:41 +0300

    responsive2

--- evdis/Scripts/mediaqueries.js
+++ evdis/Scripts/mediaqueries.js
@@ -1,32 +1,50 @@
-var qm =window.matchMedia( "(max-width: 1024px)" );
-
-if(qm.matches)
-     {
-         var ekip=document.getElementById("ekip");
-         ekip.setAttribute("data-position","-30,100.7");
-         var doctor=document.getElementById("kadin");
-         doctor.setAttribute("data-position","0.8,420");
-     }
-    //  else{
-    //      var ekip=document.getElementById("ekip");
-    //      ekip.setAttribute("data-position","-20,29.7");
-    //      var doctor=document.getElementById("kadin");
-    //      doctor.setAttribute("data-position","4,60");
+var qm = window.matchMedia("(max-width: 1024px)");
+var qm2=window.matchMedia("(max-width:768px)");
+var qm3 = window.matchMedia ("(max-width:425px)");
+if (qm.matches) {
+    var ekip = document.getElementById("ekip");
+    ekip.setAttribute("data-position", "-30,100.7");
+    var doctor = document.getElementById("kadin");
+    doctor.setAttribute("data-position", "0.8,420");
 }
+if(qm2.matches){
+        var doctor = document.getElementById("kadin");
+        doctor.setAttribute("data-position", "50,450");
+    }
+    if(qm3.matches){
+        var ekip = document.getElementById("ekip");
+    ekip.setAttribute("data-position", "-30,190.7");
+    var doctor = document.getElementById("kadin");
+    doctor.setAttribute("data-position", "50,520");
+    }
+// else {
+//     var ekip = document.getElementById("ekip");
+//     ekip.setAttribute("data-position", "-100,297");
+//     var doctor = document.getElementById("kadin");
+//     doctor.setAttribute("data-position", "20,600");
+// }
 
-window.onresize=function(event)
-{
-     if(qm.matches)
-     {
+window.onresize = function (event) {
+    if (qm.matches) {
+        var ekip = document.getElementById("ekip");
+        ekip.setAttribute("data-position", "-20,9.7");
+        var doctor = document.getElementById("kadin");
+        doctor.setAttribute("data-position", "4,40");
+    }
+    if(qm2.matches){
+        var doctor = document.getElementById("kadin");
+        doctor.setAttribute("data-position", "4,40");
+    }
+     else{
          var ekip=document.getElementById("ekip");
-         ekip.setAttribute("data-position","-20,9.7");
+         ekip.setAttribute("data-position","-20,29.7");
          var doctor=document.getElementById("kadin");
-         doctor.setAttribute("data-position","4,40");
+         doctor.setAttribute("data-position","4,60");
      }
-    //  else{
-    //      var ekip=document.getElementById("ekip");
-    //      ekip.setAttribute("data-position","-20,29.7");
-    //      var doctor=document.getElementById("kadin");
-    //      doctor.setAttribute("data-position","4,60");
-    //  }
+     if(qm3.matches){
+        var ekip = document.getElementById("ekip");
+        ekip.setAttribute("data-position", "0,20.7");
+        var doctor = document.getElementById("kadin");
+        doctor.setAttribute("data-position", "20,55");
+    }
 }
